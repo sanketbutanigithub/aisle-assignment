6799fdbcad00dab0df23b52b5f69ee8bdedae4e1
sanketbutanigithub <sanketbutani36.github@gmail.com>
2023-05-24 16:53:23 +0530

    change dialog box color

--- app/src/main/java/com/example/aisle_assignment/utils/DialogUtils.java
+++ app/src/main/java/com/example/aisle_assignment/utils/DialogUtils.java
@@ -3,11 +3,14 @@ package com.example.aisle_assignment.utils;
 import android.app.ProgressDialog;
 import android.content.Context;
 
+import com.example.aisle_assignment.R;
+
 public class DialogUtils {
     public static ProgressDialog dialog;
     public static void showDialog(Context context){
-        dialog = ProgressDialog.show(context, "",
-                "Please wait...", true);
+        dialog = new ProgressDialog(context,R.style.AppCompatAlertDialogStyle);
+        dialog.setMessage("Please wait...");
+        dialog.show();
     }
 
     public static void dialogDismiss(){
